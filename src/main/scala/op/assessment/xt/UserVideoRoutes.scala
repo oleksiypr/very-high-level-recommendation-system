0b050e2c1e98fecc9e7a72f54b721bfbb8684e7d
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2018-03-18 18:29:00 +0200

    XT. Very High Level Recommendation System. UserVideo routes: POST /register: Cats validation implemented.

--- src/main/scala/op/assessment/xt/UserVideoRoutes.scala
+++ src/main/scala/op/assessment/xt/UserVideoRoutes.scala
@@ -4,7 +4,11 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import op.assessment.sn.JsonSupport
-import op.assessment.xt.UserVideoRoutes.{Register, User}
+import op.assessment.xt.UserVideoRoutes.{Errors, Register, User, UserValidation}
+import cats.data._
+import cats.data.Validated._
+import cats.implicits._
+import op.assessment.xt.UserVideoRoutes.UserValidation.ValidationResult
 
 object UserVideoRoutes {
 
@@ -24,6 +28,43 @@ object UserVideoRoutes {
     )
 
   final case class Errors(errs: List[String])
+
+  sealed abstract class UserValidation(val message: String)
+  case object EmailNotValid extends UserValidation("email is not valid")
+  case object AgeNotValid extends UserValidation("age is not valid")
+  case object GenderNotValid extends UserValidation("gender is not valid")
+
+  object UserValidation {
+
+    type ValidationResult[A] = ValidatedNel[UserValidation, A]
+
+    def validate(user: User): ValidationResult[User] = (
+        validatenName(user.name),
+        validateEmail(user.email),
+        valiadteAge(user.age),
+        validateGender(user.gender)
+      ).mapN(User)
+
+    private def validatenName(name: String
+      ): ValidationResult[String] = name.validNel
+
+    private def validateEmail(
+        email: String
+      ): ValidationResult[String] = {
+      if ("""(\w+)@([\w\.]+)""".r.unapplySeq(email).isDefined) email.validNel
+      else EmailNotValid.invalidNel
+    }
+
+    private def valiadteAge(age: Int): ValidationResult[Int] = {
+      if (age >= 5 && age <= 120) age.validNel
+      else AgeNotValid.invalidNel
+    }
+
+    private def validateGender(gender: Int): ValidationResult[Int] = {
+      if (gender == 1 || gender == 2) gender.validNel
+      else GenderNotValid.invalidNel
+    }
+  }
 }
 
 trait UserVideoRoutes extends JsonSupport {
@@ -31,7 +72,17 @@ trait UserVideoRoutes extends JsonSupport {
   lazy val routes: Route = path("register") {
     post {
       entity(as[User]) { u =>
-        complete((StatusCodes.OK, Register(userId = 9797345L, videoId = 4324556L)))
+        val validated: ValidationResult[User] = UserValidation.validate(u)
+        validated match {
+          case Valid(user) =>
+            complete((
+              StatusCodes.OK,
+              Register(userId = 9797345L, videoId = 4324556L)))
+          case Invalid(errors) =>
+            complete((
+              StatusCodes.BadRequest,
+              Errors(errors.toList.map(_.message))))
+        }
       }
     }
   }
--- src/test/scala/op/assessment/xt/UserVideoRoutesSpec.scala
+++ src/test/scala/op/assessment/xt/UserVideoRoutesSpec.scala
@@ -3,7 +3,7 @@ package op.assessment.xt
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.{MessageEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import op.assessment.xt.UserVideoRoutes.{Register, User}
+import op.assessment.xt.UserVideoRoutes.{Errors, Register, User}
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.concurrent.ScalaFutures
 
@@ -32,13 +32,64 @@ class UserVideoRoutesSpec extends WordSpec
       }
     }
     "return 400: email is not valid " in {
+      val  request = Post(
+        "/register"
+      ).withEntity(
+        Marshal(User(
+          name = "David",
+          email = "david-gmail.com",
+          age = 28,
+          gender = 1
+        )).to[MessageEntity].futureValue
+      )
 
+      request ~> routes ~> check {
+        status should ===(StatusCodes.BadRequest)
+        entityAs[Errors] should ===(
+          Errors(List("email is not valid"))
+        )
+      }
     }
     "return 400: age is not valid " in {
+      val  request = Post(
+        "/register"
+      ).withEntity(
+        Marshal(User(
+          name = "David",
+          email = "david@gmail.com",
+          age = 127,
+          gender = 1
+        )).to[MessageEntity].futureValue
+      )
 
+      request ~> routes ~> check {
+        status should ===(StatusCodes.BadRequest)
+        entityAs[Errors] should ===(
+          Errors(List("age is not valid"))
+        )
+      }
     }
-    "return 400: gender is not valid " in {
+    "return 400: email, gender and age are not valid " in {
+      val  request = Post(
+        "/register"
+      ).withEntity(
+        Marshal(User(
+          name = "David",
+          email = "david-gmail.com",
+          age = 127,
+          gender = 3
+        )).to[MessageEntity].futureValue
+      )
 
+      request ~> routes ~> check {
+        status should ===(StatusCodes.BadRequest)
+        entityAs[Errors] should ===(
+          Errors(List(
+            "email is not valid",
+            "age is not valid",
+            "gender is not valid"))
+        )
+      }
     }
   }
 }
